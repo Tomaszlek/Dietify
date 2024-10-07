fbc31a6f0f24e26d34c4ff0b1692f4c93dd3f466
Tomek <Tomek@DESKTOP-DRK49CI>
2024-10-07 14:57:14 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var table = new Table();
+        table.AddColumn("Column 1");
+        table.AddColumn("Column 2");
+
+        table.AddRow("Cell 1", "Cell 2");
+        table.AddRow("Cell 3", "Cell 4");
+
+        AnsiConsole.Write(table);
+    }
+}
